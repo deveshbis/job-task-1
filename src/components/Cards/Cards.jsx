01fe37e8b50b779f87223021faa0ebee7bdc5f72
deveshbis <deveshbiswas.cse@gmail.com>
2024-08-17 09:25:39 +0600

    Implement sorting functionality Date Added: Newest first

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,88 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-
-// const Cards = () => {
-//     const [card, setCard] = useState([])
-
-//     useEffect(() => {
-//         fetch("http://localhost:5000/products")
-//             .then(res => res.json())
-//             .then(data => setCard(data)
-//             )
-//     }, [])
-
-//     return (
-//         <div>
-//             <div className="font-[sans-serif] py-4 mx-auto lg:max-w-7xl sm:max-w-full">
-//                 <h2 className="text-4xl font-extrabold text-gray-800 mb-12">Premium Sneakers</h2>
-//                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-
-//                     {
-//                         card.map(aCard => <div key={aCard.ProductId}
-//                             className="bg-gray-50 shadow-md overflow-hidden rounded-lg cursor-pointer hover:-translate-y-2 transition-all relative">
-//                             <div
-//                                 class="bg-gray-100 w-20 h-20 flex items-center justify-center rounded-full cursor-pointer absolute top-3 right-3">
-//                                 {aCard.BrandName}
-
-//                             </div>
-//                             <div className="w-5/6 h-[260px] p-4 overflow-hidden mx-auto aspect-w-16 aspect-h-8">
-//                                 <img src={aCard.ProductImage} alt="Product 1"
-//                                     className="h-full w-full object-contain" />
-//                             </div>
-
-//                             <div className="p-6 bg-white">
-//                                 <h3 className="text-lg font-bold text-gray-800">{aCard.ProductName}</h3>
-//                                 <h4 className="text-lg text-gray-800 font-bold mt-2">{aCard.Price}</h4>
-//                                 <h3 className="text-lg text-gray-800 font-bold ">Category: {aCard.Category}</h3>
-//                                 <p className="text-gray-600 text-sm mt-2">{aCard.Description}</p>
-
-//                                 <div className="flex space-x-2 mt-4">
-//                                     Ratings
-//                                     <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                                         <path
-//                                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-//                                     </svg>
-//                                     <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                                         <path
-//                                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-//                                     </svg>
-//                                     <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                                         <path
-//                                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-//                                     </svg>
-//                                     <svg className="w-4 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                                         <path
-//                                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-//                                     </svg>
-//                                     <svg className="w-4 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                                         <path
-//                                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-//                                     </svg>
-//                                     {aCard.Ratings}
-//                                 </div>
-//                             </div>
-//                         </div>)
-//                     }
-
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Cards;
-
-
-
-
-
-
-
-
-
-
-
-
-
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
@@ -203,8 +118,8 @@ const Cards = () => {
                             <option value='Digital Marketing'>Digital Marketing</option>
                         </select>
                     </div>
-
-
+                </div>
+                <div className='flex flex-col md:flex-row justify-center items-center gap-5 mt-5'>
                     <form onSubmit={handleSearch}>
                         <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
                             <input
@@ -222,6 +137,8 @@ const Cards = () => {
                             </button>
                         </div>
                     </form>
+                </div>
+                <div className='flex flex-col md:flex-row justify-center items-center gap-5 mt-5'>
                     <div>
                         <select
                             onChange={e => {
@@ -234,9 +151,24 @@ const Cards = () => {
                             className='border p-4 rounded-md'
                         >
                             <option value=''>Sort By Price Range</option>
-                            <option value='dsc'>Price: High to High</option>
-                            <option value='asc'>Price: Low to Low</option>
+                            <option value='dsc'>Price: High to Low</option>
+                            <option value='asc'>Price: Low to High</option>
                             {/* <option value='Newest first'>Date Added: Newest first</option> */}
+                        </select>
+                    </div>
+                    <div>
+                        <select
+                            onChange={e => {
+                                setSort(e.target.value)
+                                setCurrentPage(1)
+                            }}
+                            value={sort}
+                            name='sort'
+                            id='sort'
+                            className='border p-4 rounded-md'
+                        >
+                            <option value=''>Sort By Date</option>
+                            <option value='dsc'>Newest first</option>
                         </select>
                     </div>
                 </div>
@@ -260,6 +192,7 @@ const Cards = () => {
                                 <h4 className="text-lg text-gray-800 font-bold mt-2">${aCard.Price}</h4>
                                 <h3 className="text-lg text-gray-800 font-bold ">Category: {aCard.Category}</h3>
                                 <p className="text-gray-600 text-sm mt-2">{aCard.Description}</p>
+                                <p className="text-gray-600 text-sm mt-2">{aCard.CreationDateTime}</p>
 
                                 <div className="flex space-x-2 mt-4">
                                     Ratings
